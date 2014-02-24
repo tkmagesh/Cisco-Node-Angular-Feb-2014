4ab011909b2bde91071e47ded63f1b22f99d8214
tkmagesh <tkmagesh77@gmail.com>
2014-02-24 12:19:31 -0800

    Calculator - Final

--- Day-1/JavaScript-Primer/calculator.js
+++ Day-1/JavaScript-Primer/calculator.js
@@ -2,16 +2,10 @@ function sum(){
 	function parseArg(n){
 		if (!isNaN(n)) return parseInt(n);	
 		if (typeof n === "function") return parseArg(n());
-		if (!!n.length){
-			var result = 0;
-			for(var i=0;i<n.length;i++)
-				result += parseArg(n[i]);
-			return result;		
-		}
+		if (!!n.length) return sum.apply(this,n);
 		return 0;
 	}
-	var result = 0;
-	for(var i=0;i<arguments.length;i++)
-		result += parseArg(arguments[i]);
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) 
+		: parseArg(arguments[0]) 
+		+ sum.apply(this,[].slice.call(arguments,1));
 }
